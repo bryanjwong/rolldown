06ea88d35d4a7cc6f4c4b34168168f3aad69c769
Bryan Wong <bryanjwong@g.ucla.edu>
2020-03-24 11:11:36 -0700

    completed react tutorial

--- practice.js
+++ practice.js
@@ -1,61 +1,104 @@
 var todoList = {
   todos: [],
-  displayTodos: function() {
-    if(this.todos.length === 0) {
-      console.log("My Todo list is empty!");
-    }
-    else {
-      console.log("My Todos:");
-      this.todos.forEach((todo, i) => {
-        var checkbox = "";
-        if(todo.completed === true) {
-          checkbox = "(x)";
-        }
-        else {
-          checkbox = "( )";
-        }
-        console.log(checkbox, todo.todoText);
-      });
-    }
-  },
-  addTodos: function(todoText) {
+  addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
   },
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
-    this.displayTodos();
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
-    this.displayTodos();
   },
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   toggleAll: function() {
     var allCompleted = this.todos.every(function(todo) {
       return todo.completed;
     });
     var toggleValue = allCompleted ? false : true;
-    for(let todo of this.todos) {
+    this.todos.forEach(function(todo) {
       todo.completed = toggleValue;
-    }
-    this.displayTodos();
+    });
+
   }
 };
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-displayTodosButton.addEventListener('click', function() {
-  todoList.displayTodos();
-});
+var handlers = {
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = "";
+    view.displayTodos();
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value = "";
+    changeTodoTextInput.value = "";
+    view.displayTodos();
+  },
+  deleteTodo: function(position) {
+    todoList.deleteTodo(position);
+    view.displayTodos();
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value = "";
+    view.displayTodos();
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+    view.displayTodos();
+  }
+};
 
-var toggleTodosButton = document.getElementById('toggleTodosButton');
-toggleTodosButton.addEventListener('click', function() {
-  todoList.toggleAll();
-});
+var view = {
+  displayTodos: function() {
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = "";
+    todoList.todos.forEach((todo, i) => {
+      var checkbox = "";
+      if(todo.completed === true) {
+        checkbox = "(x) ";
+      }
+      else {
+        checkbox = "( ) ";
+      }
+      var todoLi = document.createElement('li');
+      todoLi.id = i;
+      todoLi.textContent = checkbox + todo.todoText;
+      todoLi.appendChild(this.createDeleteButton(i));
+      todosUl.appendChild(todoLi);
+    });
+  },
+  createDeleteButton: function(i) {
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+    deleteButton.className = 'deleteButton';
+    return deleteButton;
+  },
+  setupEventListeners: function() {
+    var todosUl = document.querySelector('ul');
+    todosUl.addEventListener('click', function(event) {
+      var elementClicked = event.target;
+      if(elementClicked.className == 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+        view.displayTodos();
+      }
+    });
+  }
+};
+
+view.setupEventListeners();
+
+function runWithDebugger(ourFunction) {
+  debugger;
+  ourFunction();
+}
