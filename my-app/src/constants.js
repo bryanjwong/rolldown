692d273fc148f6a5935e303e58bdda7284e07fb5
Bryan Wong <bryanjwong@g.ucla.edu>
2020-03-25 21:14:59 -0700

    added reroll system

---
+++ my-app/src/constants.js
@@ -0,0 +1,34 @@
+export const XP_THRESH = [-1, -1, 2, 6, 10, 20, 32, 50, 66]
+export const REROLL_ODDS = {
+  2: [100, 0, 0, 0, 0],
+  3: [70, 30, 0, 0, 0],
+  4: [50, 35, 15, 0, 0],
+  5: [35, 40, 20, 0, 0],
+  6: [20, 35, 35, 10, 0],
+  7: [14, 30, 40, 15, 1],
+  8: [13, 20, 35, 25, 7],
+  9: [10, 15, 25, 35, 15]
+}
+const CHAMPION_TOTAL_AMOUNT = {
+  1: 29,
+  2: 22,
+  3: 16,
+  4: 12,
+  5: 10
+}
+
+const ChampionData = require("./json/champions.json");
+export var championPool = {
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: []
+};
+
+ChampionData.forEach((champion, i) => {
+  let cost = champion.cost;
+  for(let i = 0; i < CHAMPION_TOTAL_AMOUNT[cost]; i++) {
+    championPool[cost].push(champion);
+  }
+})
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,65 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as Constants from './constants/constants.js';
-import gangplank from "./images/gangplank.jpeg";
-import malphite from "./images/renders/malphite.png";
-import gem1 from "./images/1-cost-gem.png";
-import gem2 from "./images/2-cost-gem.png";
-import gem3 from "./images/3-cost-gem.png";
-import gem4 from "./images/4-cost-gem.png";
-import gem5 from "./images/5-cost-gem.png";
-import championtile1 from "./images/champion-tile-1.png";
-import championtilehovered1 from "./images/champion-tile-hovered-1.png"
-import UIBuyXP from "./images/buy-xp.png"
-import UIBuyXPHovered from "./images/buy-xp-hovered.png"
-import UIReroll from "./images/reroll.png"
-import UIRerollHovered from "./images/reroll-hovered.png"
-import Gold from "./images/gold.png"
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import * as Constants from "./constants.js"
+const ChampionData = require("./json/champions.json");
 
-function ChampionTile(props) {
-  function handleMouseOver(e) {
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+const images = importAll(require.context('./images', true, /\.(png|jpe?g|svg)$/));
+console.log(images);
+console.log(ChampionData);
+console.log(Constants.championPool);
+
+class ChampionTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bought: false
+    };
+  }
+  handleMouseOver(e) {
     var champTile = e.target.getElementsByClassName("champ-tile-window")[0];
     if(champTile) {
-      champTile.src = championtilehovered1;
+      champTile.src = images['champion-tile-hovered-1.png'];
     }
   }
-
-  function handleMouseLeave(e) {
+  handleMouseLeave(e) {
     var champTile = e.target.getElementsByClassName("champ-tile-window")[0];
     if(champTile) {
-      champTile.src = championtile1;
+      champTile.src = images['champion-tile-1.png'];
     }
   }
-
-  return (
-    <div
-      className="btn btn-primary shop-tile clickable"
-      onMouseOver={handleMouseOver}
-      onMouseLeave={handleMouseLeave}
-    >
-      <img className="champ-pic" src={gangplank} />
-      <img className="champ-tile-window" src={championtile1}/>
-      <div width="100%">
-        <div className="d-inline sm-font champ-name">{props.championName}</div>
-        <div className="d-inline sm-font champ-cost"><img className="d-inline gold-icon-sm" src={Gold}/>{props.championCost}</div>
+  render() {
+    return (
+      <div
+        className="shop-tile clickable"
+        onMouseOver={this.handleMouseOver}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <img className="champ-pic" src={images['splash/gangplank.jpeg']} />
+        <img className="champ-tile-window" src={images['champion-tile-1.png']}/>
+        <div className="champ-tile-text">
+          <div className="d-inline sm-font champ-name">{this.props.championName}</div>
+          <div className="d-inline sm-font champ-cost"><img className="d-inline gold-icon-sm" src={images['gold.png']}/>{this.props.championCost}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 function BuyXPButton(props) {
   function handleMouseOver(e) {
     var buttonUI = e.target.getElementsByClassName("left-btn-ui")[0];
     if(buttonUI) {
-      buttonUI.src = UIBuyXPHovered;
+      buttonUI.src = images['buy-xp-hovered.png'];
     }
   }
 
   function handleMouseLeave(e) {
     var buttonUI = e.target.getElementsByClassName("left-btn-ui")[0];
     if(buttonUI) {
-      buttonUI.src = UIBuyXP;
+      buttonUI.src = images['buy-xp.png'];
     }
   }
 
@@ -70,10 +73,10 @@ function BuyXPButton(props) {
       onMouseOver={handleMouseOver}
       onMouseLeave={handleMouseLeave}
     >
-      <img className="left-btn-ui" src={UIBuyXP} />
+      <img className="left-btn-ui" src={images['buy-xp.png']} />
       <div className="left-btn-text">
         <div className="med-font">Buy XP</div>
-        <img className="d-inline gold-icon-sm" src={Gold}/><div className="d-inline sm-font">4</div>
+        <img className="d-inline gold-icon-sm" src={images['gold.png']}/><div className="d-inline sm-font">4</div>
       </div>
     </button>
   );
@@ -83,14 +86,14 @@ function RefreshButton(props) {
   function handleMouseOver(e) {
     var buttonUI = e.target.getElementsByClassName("left-btn-ui")[0];
     if(buttonUI) {
-      buttonUI.src = UIRerollHovered;
+      buttonUI.src = images['refresh-hovered.png'];
     }
   }
 
   function handleMouseLeave(e) {
     var buttonUI = e.target.getElementsByClassName("left-btn-ui")[0];
     if(buttonUI) {
-      buttonUI.src = UIReroll;
+      buttonUI.src = images['refresh.png'];
     }
   }
 
@@ -102,10 +105,10 @@ function RefreshButton(props) {
       onMouseOver={handleMouseOver}
       onMouseLeave={handleMouseLeave}
     >
-      <img className="left-btn-ui" src={UIReroll} />
+      <img className="left-btn-ui" src={images['refresh.png']} />
       <div className="left-btn-text">
         <div className="med-font">Refresh</div>
-        <img className="d-inline gold-icon-sm" src={Gold}/><div className="d-inline sm-font">2</div>
+        <img className="d-inline gold-icon-sm" src={images['gold.png']}/><div className="d-inline sm-font">2</div>
       </div>
     </button>
   );
@@ -115,11 +118,11 @@ function RerollOdds(props) {
   const level = props.level;
   return (
     <div className="d-inline reroll-odds">
-      <img className="reroll-gem" src={gem1}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][0]}%</h6>
-      <img className="reroll-gem" src={gem2}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][1]}%</h6>
-      <img className="reroll-gem" src={gem3}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][2]}%</h6>
-      <img className="reroll-gem" src={gem4}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][3]}%</h6>
-      <img className="reroll-gem" src={gem5}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][4]}%</h6>
+      <img className="reroll-gem" src={images['gem-1.png']}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][0]}%</h6>
+      <img className="reroll-gem" src={images['gem-2.png']}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][1]}%</h6>
+      <img className="reroll-gem" src={images['gem-3.png']}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][2]}%</h6>
+      <img className="reroll-gem" src={images['gem-4.png']}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][3]}%</h6>
+      <img className="reroll-gem" src={images['gem-5.png']}/><h6 className="d-inline sm-font">{Constants.REROLL_ODDS[level][4]}%</h6>
     </div>
   )
 }
@@ -127,15 +130,23 @@ function RerollOdds(props) {
 class Shop extends React.Component {
   constructor(props) {
     super(props);
+    let myStore = [];
+    for(let i = 0; i < 5; i++) {
+      myStore.push({
+        champion: "",
+        cost: ""
+      })
+    }
     this.state = {
       level: 2,
       xp: 0,
-      gold: 999
+      gold: 999,
+      store: myStore
     };
   }
 
   buyXPClicked() {
-    if(this.state['level'] == 9 || this.state['gold'] < 4) {
+    if(this.state['level'] === 9 || this.state['gold'] < 4) {
       return;
     }
     var gold = this.state['gold'] - 4;
@@ -156,15 +167,44 @@ class Shop extends React.Component {
     if(this.state['gold'] < 2) {
       return;
     }
-    var gold = this.state['gold'] - 2;
+    let gold = this.state['gold'] - 2;
+    let myStore = [];
+    for(let i = 0; i < 5; i++) {
+      let champRolled = this.reroll();
+      myStore.push({
+        champion: champRolled['name'],
+        cost: champRolled['cost']
+      });
+    }
     this.setState ({
-      gold: gold
+      gold: gold,
+      store: myStore
     });
+  }
+
+  reroll() {
+    let randCost = Math.floor(Math.random() * 100);
+    let level = this.state.level;
+    let costRolled;
+
+    if(randCost === 0) {
+      costRolled = 1;
+    }
+    else {
+      for(var i = 0, runningPercentage = 0; randCost > runningPercentage; i++) {
+        runningPercentage += Constants.REROLL_ODDS[level][i];
+      }
+      costRolled = i;
+    }
+    let randChamp = Math.floor(Math.random() * Constants.championPool[costRolled].length);
+    let rolledChamp = Constants.championPool[costRolled][randChamp];
+    Constants.championPool[costRolled].splice(randChamp, 1);
+    return rolledChamp;
   }
 
   render() {
     const level = this.state['level'];
-    const xp_text = (level == 9) ? "Max" : this.state['xp'] + "/" + Constants.XP_THRESH[level];
+    const xp_text = (level === 9) ? "Max" : this.state['xp'] + "/" + Constants.XP_THRESH[level];
     return (
       <div className="shop">
 
@@ -172,7 +212,7 @@ class Shop extends React.Component {
           <h2 className="level d-inline lrg-font">Lvl.{this.state['level']}</h2>
           <h5 className="exp d-inline med-font">{xp_text}</h5>
           <RerollOdds level={this.state['level']}/>
-          <div className="gold d-inline lrg-font"><img className="d-inline gold-icon-lrg" src={Gold}/>{this.state['gold']}</div>
+          <div className="gold d-inline lrg-font"><img className="d-inline gold-icon-lrg" src={images['gold.png']}/>{this.state['gold']}</div>
         </div>
 
         <div>
@@ -180,11 +220,11 @@ class Shop extends React.Component {
             <div><BuyXPButton onClick={() => this.buyXPClicked()}/></div>
             <div><RefreshButton onClick={() => this.refreshClicked()}/></div>
           </div>
-          <ChampionTile championName="Gangplank" championCost="5"/>
-          <ChampionTile championName="Lucian" championCost="5"/>
-          <ChampionTile championName="Kayle" championCost="5"/>
-          <ChampionTile championName="Cho'gath" championCost="5"/>
-          <ChampionTile championName="Kha'zix" championCost="5"/>
+          <ChampionTile championName="Gangplank" championCost="5" />
+          <ChampionTile championName="Lucian" championCost="5" />
+          <ChampionTile championName="Kayle" championCost="5" />
+          <ChampionTile championName="Cho'gath" championCost="5" />
+          <ChampionTile championName="Kha'zix" championCost="5" />
         </div>
 
       </div>
@@ -194,7 +234,7 @@ class Shop extends React.Component {
 
 function ChampionStageTile(props) {
   return(
-    <img className="champ-stage-tile" src={malphite} />
+    <img className="champ-stage-tile" src={images['renders/malphite.png']} />
   );
 }
 
